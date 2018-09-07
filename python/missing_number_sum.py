241db5d1d3fcf2afd1bd624bbe5758c330dc0a09
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2018-09-07 11:49:36 -0500

    Update missing_number_sum.py

--- python/missing_number_sum.py
+++ python/missing_number_sum.py
@@ -1,5 +1,8 @@
 # Time Complexity - O(n)
 
+#  1.  Get the sum of numbers, eg:  total = n*(n+1)/2
+#  2.  Subtract all the numbers from sum and you will get the missing number
+
 def get_missing_number(list):
     n = len(list)
     total = (n+1)*(n+2)/2
